cc62eb336c396bf83f9413b1f66535a6912f127d
jzfrank <frankjinzhang@gmail.com>
2022-09-01 15:07:35 +0200

    created body section

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-  <Component {...pageProps} />
-
+      <Component {...pageProps} />
     </SessionProvider>
-  )
-  
+  );
 }
 
 export default MyApp;
