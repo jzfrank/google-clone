be688c28dcecb24c9e333aa96a2e79d052521862
jzfrank <frankjinzhang@gmail.com>
2022-09-02 14:30:04 +0200

    created pagination component

--- pages/search.js
+++ pages/search.js
@@ -27,13 +27,14 @@ const search = ({ results }) => {
 export async function getServerSideProps(context) {
   // have a mock data while styling, otherwise you
   // will easily use up the quota of API
-  const mockData = true;
+  const startIndex = context.query.start || "1";
+  const mockData = false;
   const data = mockData
     ? Response
     : await fetch(`https://www.googleapis.com/customsearch/v1?key=${
         process.env.API_KEY
       }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}
-    ${context.query.searchType && "&searchType=image"}
+    ${context.query.searchType && "&searchType=image"}&start=${startIndex}
   `).then((response) => response.json());
   return {
     props: {
